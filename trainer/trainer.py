d613be2a23c865c2565549dcdd7a15e054c6792a
yjlolo <yjlolo125@gmail.com>
2020-08-28 15:49:34 +0800

    add sigmoid to generation plot

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -240,6 +240,7 @@ class Trainer(BaseTrainer):
 
             n_sample = 3
             output_seq, z_p_seq, mu_p_seq, logvar_p_seq = self.model.generate(n_sample, 100)
+            output_seq = torch.sigmoid(output_seq)
             plt.close()
             fig, ax = plt.subplots(n_sample, 1, figsize=(10, n_sample * 10))
             for i in range(n_sample):
@@ -276,7 +277,6 @@ def determine_annealing_factor(min_anneal_factor,
 
 def create_reconstruction_figure(x, x_recon, sample=True):
     plt.close()
-    print(x.shape, x_recon.shape)
     if sample:
         idx = np.random.choice(x.shape[0], 1)[0]
     else:
